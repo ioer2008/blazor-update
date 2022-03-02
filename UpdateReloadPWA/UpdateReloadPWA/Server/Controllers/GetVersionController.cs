d91c7c4c7d40770a8efa49cd0298a4cc05db824a
郭军 <ioer@live.com>
2022-03-02 22:47:11 +0800

    Add files via upload

---
+++ UpdateReloadPWA/UpdateReloadPWA/Server/Controllers/GetVersionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace UpdateReloadPWA.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetVersionController : ControllerBase
+    {
+        IConfiguration _configuration;
+
+        public GetVersionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> Get()
+        {
+            await Task.Delay(0);
+            return _configuration["AppVersion"];
+        }
+    }
+}
---
+++ UpdateReloadWasm/UpdateReloadWasm/Client/AppState.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpdateReloadWasm.Client
+{
+    public class AppState
+    {
+        public int Counter { get; set; } = 0;
+        public int TimeToLiveInSeconds { get; set; } = 180;
+        public DateTime LastAccessed { get; set; } = DateTime.Now;
+
+    }
+}
